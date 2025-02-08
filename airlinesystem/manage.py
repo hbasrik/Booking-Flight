a3ef7d7e23fb1eeac1792f77429b1dbd03b009d7
hbasrik <basri.kocagol@ug.bilkent.edu.tr>
2025-02-08 21:59:22 +0300

    Manage.py is edited based on the env

--- airlinesystem/manage.py
+++ airlinesystem/manage.py
@@ -2,11 +2,16 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from airlinesystem.settings import base
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airlinesystem.settings')
+    if base.DEBUG:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airlinesystem.settings.local')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airlinesystem.settings.production')
+
+        
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
