592df45a6d29129be4321a95ae72ed6e542b8377
hbasrik <basri.kocagol@ug.bilkent.edu.tr>
2025-02-10 01:22:17 +0300

    Serializers are defined

---
+++ airlinesystem/api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Airplane, Flight, Reservation
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airplane
+        fields = "__all__"
+
+
+class FlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flight
+        fields = "__all__"
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = "__all__"  # spesicially array can be defined
