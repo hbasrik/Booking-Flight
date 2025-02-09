caf363a2c1f6f7398a2d66035b17547b75ef7fe7
hbasrik <basri.kocagol@ug.bilkent.edu.tr>
2025-02-10 01:11:12 +0300

    Models are defined

--- airlinesystem/api/models.py
+++ airlinesystem/api/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+import random
+import string
 
 # Create your models here.
+
+
+class Airplan(models.model):
+    tail_number = models.CharField(max_length=6, unique=True, primary_key=True)
+    model = models.CharField(max_length=50)
+    capacity = models.IntegerField()
+    production_year = models.IntegerField()
+    status = models.BooleanField(default=True)
+    # PROTECT couldb be used if Airplane is still used in a Flight.
+
+    def __str__(self):
+        return f"{self.model} ({self.tail_number})"
+
+
+class Flight(models.model):
+    flight_number = models.CharField(max_length=10)
+    departure = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    airplane = models.ForeignKey(
+        Airplan,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+        related_name="flights",
+    )
+
+    def __str__(self):
+        return f" Flight {self.flight_number} ({self.departure} to {self.destination})"
+
+
+class Reservation(models.Model):
+    passenger_name = models.CharField(max_length=100)
+    passenger_email = models.EmailField()
+    reservation_code = models.CharField(max_length=10)
+    flight = models.ForeignKey(
+        Flight,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name="reservations",
+    )
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Reservation {self.reservation_code} for {self.passenger_name}"
+
+
+def save(self, *args, **kwargs):
+
+    if not self.reservation_code:
+        self.reservation_code = self.generate_reservation_code()
+    super().save(*args, **kwargs)
+
+
+def generate_reservation_code(self):
+
+    characters = string.ascii_uppercase + string.digits
+    code = "".join(random.choices(characters, k=6))
+
+    while Reservation.objects.filter(reservation_code=code).exists():
+        code = "".join(random.choices(characters, k=6))
+
+    return code
