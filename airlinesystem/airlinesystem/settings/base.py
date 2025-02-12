583d7b10eee7d8a23135b1d9ede799fdfe8ca6e2
hbasrik <basri.kocagol@ug.bilkent.edu.tr>
2025-02-12 22:05:02 +0300

    Heroku deploy issues are trying to be fixed

--- airlinesystem/airlinesystem/settings/base.py
+++ airlinesystem/airlinesystem/settings/base.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 # CSRF_TRUSTED_ORIGINS = ["http://*.on-acorn.io", "https://*.on-acorn.io"]
